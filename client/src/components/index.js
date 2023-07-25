0cf8cd42e39618aadf5e6cf441d164d70c745217
winklevi1 <gavan.browne@gmail.com>
2023-07-25 20:51:53 +0100

    Deleted more unnecessary code from frontend.

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -5,14 +5,11 @@ import Logo from "./Logo.js";
 import SmallSidebar from "./SmallSidebar.js";
 import Navbar from "./Navbar.js";
 import FormRowSelect from "./FormRowSelect.js";
-import StatItem from "./StatItem.js";
 import ChartsContainer from "./ChartsContainer.js";
-import StatsContainer from "./StatsContainer.js";
 import Loading from "./Loading.js";
 import ModalAlert from "./ModalAlert.js";
 import NoData from "./NoData.js";
 import QuestionListItem from "./QuestionListItem.js";
-import SearchContainer from "./SearchContainer.js";
 import CategoryListItem from "./CategoryListItem.js";
 import TestButtons from "./TestButtons.js";
 import ResultsButtons from "./ResultsButtons.js";
@@ -34,9 +31,6 @@ export {
   BigSidebar,
   Navbar,
   FormRowSelect,
-  SearchContainer,
-  StatsContainer,
-  StatItem,
   ChartsContainer,
   NoData,
   QuestionListItem,
